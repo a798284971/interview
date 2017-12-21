b51bb54dda4ae96349bffa2334cfe6cb66f9f5d4
hegeyang <a798284971@qq.com>
2017-12-21 09:24:13 +0800

    Initial commit

---
+++ app/src/main/java/com/hevttc/jdr/interiew/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hevttc.jdr.interiew.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hevttc.jdr.interiew.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
