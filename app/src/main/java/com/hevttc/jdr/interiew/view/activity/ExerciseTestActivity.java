46d4d769b08e496e76f78cf4aba961608ac20045
hegeyang <duyaobuku@163.com>
2018-04-09 21:46:25 +0800

    增加ExerciseTestActivity 完善布局 2018-4-9 21:46:17

---
+++ app/src/main/java/com/hevttc/jdr/interiew/view/activity/ExerciseTestActivity.java
@@ -0,0 +1,22 @@
+package com.hevttc.jdr.interiew.view.activity;
+
+/**
+ * Created by hegeyang on 2018/4/9.
+ */
+
+public class ExerciseTestActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
