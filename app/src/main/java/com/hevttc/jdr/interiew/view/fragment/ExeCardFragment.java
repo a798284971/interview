d5f2f814c8e3dd480f9bb901c823283d3031f0fc
hegeyang <duyaobuku@163.com>
2018-04-25 18:17:05 +0800

    答题卡布局

---
+++ app/src/main/java/com/hevttc/jdr/interiew/view/fragment/ExeCardFragment.java
@@ -0,0 +1,26 @@
+package com.hevttc.jdr.interiew.view.fragment;
+
+import android.view.View;
+
+import com.hevttc.jdr.interiew.R;
+
+/**
+ * Created by hegeyang on 2018/4/25.
+ */
+
+public class ExeCardFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_execard;
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+
+    @Override
+    public void initViews(View view) {
+
+    }
+}
