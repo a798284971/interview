656ad72a43253338fe17e0729335a34333ddf9e7
hegeyang <duyaobuku@163.com>
2018-05-08 19:13:55 +0800

    test

--- app/src/main/java/com/hevttc/jdr/interiew/view/fragment/HomeFragment.java
+++ app/src/main/java/com/hevttc/jdr/interiew/view/fragment/HomeFragment.java
@@ -134,7 +134,7 @@ public class HomeFragment extends BaseFragment {
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
        // oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
         // url在微信、微博，Facebook等平台中使用
-        oks.setUrl("http://www.baidu.com");
+        oks.setUrl("http://sharesdk.cn");
         // comment是我对这条分享的评论，仅在人人网使用
         //oks.setComment("我是测试评论文本");
         // 启动分享GUI
